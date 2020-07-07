b03b35bba5e8bd5f640693bb3369d6bdbadcae00
Aelpecyem <aelpecyem@gmail.com>
2020-07-07 15:29:01 +0200

    simplified nexus

--- src/main/java/de/aelpecyem/elementaristics/common/entity/EntityNexus.java
+++ src/main/java/de/aelpecyem/elementaristics/common/entity/EntityNexus.java
@@ -1,13 +1,25 @@
 package de.aelpecyem.elementaristics.common.entity;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.mob.MobEntityWithAi;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.Packet;
+import net.minecraft.network.packet.s2c.play.EntitySpawnS2CPacket;
 import net.minecraft.world.World;
 
-public class EntityNexus extends MobEntityWithAi {
-    //nexus can spawn, but again only server side... maybe see how the packet is used in item entity
-    public EntityNexus(EntityType<? extends MobEntityWithAi> entityType, World world) {
+public class EntityNexus extends Entity {
+    public EntityNexus(EntityType<? extends Entity> entityType, World world) {
         super(entityType, world);
+    }
+
+    @Override
+    protected void initDataTracker() {
+
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
     }
 
     @Override
@@ -16,7 +28,22 @@ public class EntityNexus extends MobEntityWithAi {
     }
 
     @Override
+    protected void readCustomDataFromTag(CompoundTag tag) {
+
+    }
+
+    @Override
+    protected void writeCustomDataToTag(CompoundTag tag) {
+
+    }
+
+    @Override
     public boolean doesRenderOnFire() {
         return false;
+    }
+
+    @Override
+    public Packet<?> createSpawnPacket() {
+        return new EntitySpawnS2CPacket(this);
     }
 }
--- src/main/java/de/aelpecyem/elementaristics/registry/ModEntities.java
+++ src/main/java/de/aelpecyem/elementaristics/registry/ModEntities.java
@@ -6,7 +6,6 @@ import de.aelpecyem.elementaristics.lib.Util;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.rendereregistry.v1.EntityRendererRegistry;
-import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
@@ -18,7 +17,7 @@ public class ModEntities {
 
     public static void init() {
         Util.register(Registry.ENTITY_TYPE, "nexus", NEXUS);
-        FabricDefaultAttributeRegistry.register(NEXUS, EntityNexus.createMobAttributes());
+        //FabricDefaultAttributeRegistry.register(NEXUS, EntityN());
     }
 
     @Environment(EnvType.CLIENT)
